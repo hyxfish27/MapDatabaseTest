673eb8af1b82c4ee9bef680951f4efab8eeb39a8
hyxfish27 <avicky1515@gmail.com>
2021-06-08 23:13:15 +0800

    Delete place success

--- app/src/main/java/com/exercise/mapdatabasetest/MapsActivity.java
+++ app/src/main/java/com/exercise/mapdatabasetest/MapsActivity.java
@@ -7,11 +7,15 @@ import android.graphics.drawable.BitmapDrawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
@@ -19,7 +23,6 @@ import androidx.fragment.app.FragmentActivity;
 
 import com.exercise.mapdatabasetest.Model.DAUPlace;
 import com.exercise.mapdatabasetest.Model.Place;
-import com.exercise.mapdatabasetest.databinding.ActivityMapsBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,14 +30,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -45,18 +46,18 @@ import com.google.firebase.database.FirebaseDatabase;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleMap.OnMapLongClickListener {
+        GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
 
     private static final String TAG = "MapsActivity";
 
     private final HashMap<String, Marker> markers = new HashMap<>();
 
     private GoogleMap mMap;
+    View mapView;
     private Geocoder geocoder;
 
     private final int ACCESS_LOCATION_REQUEST_CODE = 10001;
@@ -71,18 +72,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        com.exercise.mapdatabasetest.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_maps);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         assert mapFragment != null;
+        mapView = mapFragment.getView();
         mapFragment.getMapAsync(this);
         geocoder = new Geocoder(this);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         //UiSettings uis = mMap.getUiSettings();
+        //uis.setMyLocationButtonEnabled(false);
         //mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         GoogleMapOptions googleMapOptions = new GoogleMapOptions();
         googleMapOptions.mapType(GoogleMap.MAP_TYPE_NORMAL)
@@ -94,14 +97,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .compassEnabled(true);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     public void onMapReady(@NotNull GoogleMap googleMap) {
+//        UiSettings uis = mMap.getUiSettings();
+
+        ImageButton myLocationBtn = findViewById(R.id.myLocationBtn);
+        myLocationBtn.setOnClickListener(v -> zoomToUserLocation());
 
         mMap = googleMap;
-
         mMap.setOnMapLongClickListener(this);
+        mMap.setOnMarkerClickListener(this);
 
-        List<Place> placeList = new ArrayList<>();
+//        if (mapView != null && mapView.findViewById(Integer.parseInt("1")) != null) {
+//            // Get the button view
+//            View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+//            // and next place it, on bottom right (as Google Maps app)
+//            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+        // position on right bottom
+//            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+//            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
+//            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+//            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
+//
+//            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_END, 0);
+//            layoutParams.addRule(RelativeLayout.ALIGN_END, 0);
+//            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+//            layoutParams.setMargins(0, 150, 0, 0);
+//            layoutParams.setMarginStart(25);
+//            rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+//            rlp.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+//            rlp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
+//            rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+//
+//            rlp.addRule(RelativeLayout.ALIGN_PARENT_END, 0);
+//            rlp.addRule(RelativeLayout.ALIGN_END, 0);
+//            rlp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+//            rlp.setMargins(30, 0, 0, 40);
+//        }
 
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
@@ -126,9 +159,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             break;
                         case 4:
                             bitmapDrawable = (BitmapDrawable)ResourcesCompat.getDrawable(getResources(),R.drawable.icon_leisure_3dmarker,null);
-                            break;
-                        case 5:
-                            bitmapDrawable = (BitmapDrawable)ResourcesCompat.getDrawable(getResources(),R.drawable.icon_sport_3dmarker,null);
                             break;
                         default:
                             bitmapDrawable = (BitmapDrawable)ResourcesCompat.getDrawable(getResources(),R.drawable.icon_sport_3dmarker,null);
@@ -160,12 +190,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onChildRemoved(@NotNull DataSnapshot dataSnapshot) {
-                // When markers are removed from
-                Marker deadMarker = markers.get(dataSnapshot.getKey());
-                assert deadMarker != null;
-                deadMarker.remove();
-
-                markers.remove(dataSnapshot.getKey());
+//                 When markers are removed from
+                if (markers.containsKey(dataSnapshot.getKey())) {
+                    Marker deadMarker = markers.get(dataSnapshot.getKey());
+                    if (deadMarker != null) {
+                        Log.d(TAG, "onChildRemoved: ");
+                        deadMarker.remove();
+                        markers.remove(dataSnapshot.getKey());
+                    } else {
+                        Log.d(TAG, "deadMarker is null");
+                    }
+                } else {
+                    Log.d(TAG, "onChildRemoved: ERROR");
+                }
+                Log.d(TAG, "onChildRemoved: Done");
             }
 
             @Override
@@ -215,13 +253,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     ACCESS_LOCATION_REQUEST_CODE);
         }
 
-        // Add a marker in Sydney and move the camera
-//        LatLng latLng = new LatLng(22.998837,120.226859);
-//        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Here");
-//        mMap.addMarker(markerOptions);
-//        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
-//        mMap.animateCamera(cameraUpdate);
-
         try {
             List<Address> addresses = geocoder.getFromLocationName("abc.xyz", 1);
             if (addresses.size() > 0) {
@@ -238,21 +269,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             e.printStackTrace();
         }
 
-        googleMap.setOnMarkerClickListener(marker -> {
-            dauPlace = new DAUPlace();
-            dauPlace.remove(marker.getTitle()).addOnSuccessListener(new OnSuccessListener<Void>() {
-                @Override
-                public void onSuccess(Void unused) {
-                    Toast.makeText(MapsActivity.this,"Successfully",Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull @NotNull Exception e) {
-                    Toast.makeText(MapsActivity.this,"Failed delete place...",Toast.LENGTH_SHORT).show();
-                }
-            });
-            return false;
-        });
+//        googleMap.setOnMarkerClickListener(marker -> {
+//            dauPlace = new DAUPlace();
+//            dauPlace.remove(marker.getTitle()).addOnSuccessListener(new OnSuccessListener<Void>() {
+//                @Override
+//                public void onSuccess(Void unused) {
+//                    //Toast.makeText(MapsActivity.this,"Successfully removed",Toast.LENGTH_SHORT).show();
+//                }
+//            }).addOnFailureListener(new OnFailureListener() {
+//                @Override
+//                public void onFailure(@NonNull @NotNull Exception e) {
+//                    //Toast.makeText(MapsActivity.this,"Failed delete place...",Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//            return false;
+//        });
+
     }
 
 
@@ -292,6 +324,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .zoom(16)
                     .build();
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//            int status = 0;
+//            if (status == 0) {
+//                MarkerOptions myPlace = new MarkerOptions()
+//                        .position(latLng);
+//                mMap.addMarker(myPlace);
+//                String mKey =
+//                status = 1;
+//            }else {
+//
+//            }
+
         });
         locationTask.addOnFailureListener(e -> Log.d(TAG, "onFailure: zoomToUserLocation Failed"));
 
@@ -304,9 +347,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 enableUserLocation();
                 zoomToUserLocation();
-            } else {
-                //We can show a dialog that permission is not granted
-            }
+            }  //We can show a dialog that permission is not granted
+
         }
     }
 
@@ -322,24 +364,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
     }
 
-
-
-    /*@Override
+    @Override
     public boolean onMarkerClick(@NonNull @NotNull Marker marker) {
-//        dauPlace = new DAUPlace();
-//        dauPlace.remove(marker.getTitle()).addOnSuccessListener(new OnSuccessListener<Void>() {
-//            @Override
-//            public void onSuccess(Void unused) {
-//                Toast.makeText(MapsActivity.this,"Successfully",Toast.LENGTH_SHORT).show();
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull @NotNull Exception e) {
-//                Toast.makeText(MapsActivity.this,"Failed delete place...",Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        String firebaseId = marker.getTitle();
-//        databaseReference.child(firebaseId).removeValue();
-//        return false;
-    }*/
+        dauPlace = new DAUPlace();
+        dauPlace.remove(marker.getTitle())
+                .addOnSuccessListener(unused -> Toast.makeText(MapsActivity.this,"Successfully deleted",Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(MapsActivity.this,"Failed delete place...",Toast.LENGTH_SHORT).show());
+        return false;
+    }
 }
